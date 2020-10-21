354c501fd50d876007a75cd7e787c7bfac60a23a
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-10-21 20:45:03 +0300

    some features and customer fixes

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import viewer from "v-viewer/src/component.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  viewer,
   render: function(h) {
     return h(App);
   }
